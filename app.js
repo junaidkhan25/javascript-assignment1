72ab2589586f7c9855e10c8b888f202006580574
Muhammad Junaid Khan <65032333+junaidkhan25@users.noreply.github.com>
2022-03-24 09:42:51 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,130 @@
+
+/////////////////////////////////////////////////////// chapter 1 /////////////////////////////////////////////////
+
+// // // alert("WELCOME");
+// // alert("Erroer! Please enter valid Password."");
+// alert("Welcome to JS Land \n Happy coding!");
+// alert("Welcome to JS Land... ");
+//  alert(   "Happy coding! \n prevent this page from additional dialogs");
+
+
+/////////////////////////////////////////////////////// chapter 2 /////////////////////////////////////////////////
+
+  
+// var userName = "";
+// var myName = "Junaid";
+// var message = "Hello world";
+// alert(message);
+// var name = prompt("enter your name");
+// var age = +prompt("enter your age");
+// var skills = prompt("enter your skills"); 
+// alert(name);
+// alert(age);
+// alert(skills);
+
+// var food = "PIZZA"
+
+// alert( "PIZZA \n PIZZ \n PIZ \n PI \n P  "  );
+
+// var email = "jjunaid2700@gmail.com"
+// alert( "My Email address is " + email);
+ 
+// var book = "A smarter way to learn JavaScript";
+// alert("I am trying to learn from the Book "+ book);
+// document.write(" <b> Ya! I can write HTML through Javascript </b> ");
+ 
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(design);
+
+/////////////////////////////////////////////////////// chapter 3 /////////////////////////////////////////////////
+
+
+// var age = +prompt("enter your age");
+// alert("I am   " + age + "  year old ");
+
+// var birthYear = +prompt("enter your birth year");
+// document.write( "MY birth Year is " + birthYear  ) ;
+// document.write("<br>");
+// document.write(" Data type of my decleared variable is number ");
+
+// var name = prompt("enter your name");
+// var title = prompt("enter product title");
+// var quantity = +prompt("enter how many quntity wants");
+// document.write(name + "  Ordered " + title + " " +  quantity);
+
+
+/////////////////////////////////////////////////////// chapter 4 ///////////////////////////////////////////////////
+
+// var name = "junaid";
+// var age  =  23;
+// var sport = "football"
+// document.write(name + age + sport);
+
+// var Name = "junaid";
+// var name = "ali";
+// var myName = "Junaid";
+// var $name = "junaid";
+// var name1 = " junaid";
+
+// var na me  = "junaid";
+// var n-ame  = "junaid";
+// var &name = "junaid";
+// var 2name = "junaid";
+// var nam)e = "junaid";
+// document.write("Variable names can only contain number, $ and __   For example $my_1stVariable"); 
+// document.write("<br>");
+// document.write  (" Variables must begin with a Letter, $ or __. For example $name, _name or name") ;  
+// document.write("<br>");
+// document.write("Variable names are case sensitive")   ;
+// document.write("<br>");
+// document.write("Variable names should not be JS")   ;
+
+/////////////////////////////////////////////////////// chapter 5 ///////////////////////////////////////////////////
+
+// var a = +prompt("enter firts num");
+// var b = +prompt("enter second num");
+// var sum = a +b ;
+// document.write("sum of " + sum);
+// var a = +prompt("enter firts num");
+// var b = +prompt("enter second num");
+// var sub= a - b ;
+// document.write("sub of " + sub);
+// var a = +prompt("enter firts num");
+// var b = +prompt("enter second num");
+// var mul= a * b ;
+// document.write("mul of " + mul);
+// var a = +prompt("enter firts num");
+// var b = +prompt("enter second num");
+// var div= a / b ;
+// document.write("div of " + div);
+
+// var a = 5;
+// document.write("Value after variable declaration is " + a);
+
+// var a = 6 ;
+// document.write("Initial value: 6");
+// a++;
+// document.write("<br>");
+// document.write("Value after increment is:" + a);
+// document.write("<br>");
+// var b = a + 8 ;
+// document.write("Value after addition is:" + b);
+// document.write("<br>");
+// b--;
+// document.write("Value after decrement is:" + b);
+// document.write("<br>");
+// var c =  b % 2 ;
+// document.write("The Remainder is:" + c);
+
+
+// var ticketPrice = 600;
+// var quantity =  +prompt("enter number of ticket");
+// var total = ticketPrice * quantity ;
+// document.write("total cost to buy  " + quantity + " tickets is  " + total +"PKR"  );
+
+
+// var c = 25;
+// var f = 108;
+// c = (f - 32) * 5 /9 ;
+// f = (c - 9/5) * 32 ;
+// document.write(c)
